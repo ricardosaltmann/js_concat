7bb229645620b76f761af68d34f790f1f931184b
RICARDO SOARES DE MOURA ALTMANN <94047528+ricardosaltmann@users.noreply.github.com>
2022-01-18 18:14:31 -0300

    Update concat.js

--- concat.js
+++ concat.js
@@ -1,7 +1,8 @@
 const salaPython = ['Paloma', 'leticia']
 
 const salaJavascript = ['Dinho', 'Ricardo']
-
+/* para juntar criar uma nova variavel, e juntar as duas array com o concat
+usando a primeira lista.concat(e informar a outra lista como parametro) */
 const salasUnificadas = salaPython.concat(salaJavascript)
 
 console.log(salasUnificadas)
